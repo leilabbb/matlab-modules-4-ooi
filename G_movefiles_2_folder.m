3712cb2ebb2186fa7fbe8e393e5871546a53b746
leilabbb <leila.ocean@gmail.com>
2016-09-08 16:12:09 -0400

    function used to move files
    move created files to a folder named after the file name being examined

---
+++ G_movefiles_2_folder.m
@@ -0,0 +1,20 @@
+function G_movefiles_2_folder(OutputDir,file_id,filea_name,fileb_name,file1_name,file2_name,file3_name,file4_name)
+
+% create an output directory with reference to file name
+output_dir = [OutputDir,file_id];
+%mkdir(output_dir);
+if exist(output_dir,'dir') ~= 0 
+   rmdir(output_dir,'s');
+   mkdir(output_dir);
+end
+
+% move to the assessed file folder
+cd(output_dir)
+movefile([OutputDir,filea_name])
+movefile([OutputDir,fileb_name])
+movefile([OutputDir,file1_name])
+movefile([OutputDir,file2_name])
+movefile([OutputDir,file3_name])
+movefile([OutputDir,file4_name])
+
+cd(OutputDir)
