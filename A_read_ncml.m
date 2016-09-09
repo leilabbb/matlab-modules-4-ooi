7b217e0332b2c875b3366f7a78a4ab2fba26a52a
leilabbb <leila.ocean@gmail.com>
2016-09-09 10:41:24 -0400

    added preferred time to the output

--- A_read_ncml.m
+++ A_read_ncml.m
@@ -13,14 +13,33 @@ function [meta,t0,ti,tvar,file_id] = A_read_ncml(url,fidb)
 
     % make a parameter array and gets its attribute
     
+        
+    NCML_par_list = {meta.Variables.Name}';
+    NCML_par_type = {meta.Variables.Datatype}';
+    
+    pref_time_ind = find(strcmp(NCML_par_list,'preferred_timestamp') == 1);
+    if ~isempty(pref_time_ind)
+        time_name = NCML_par_list(pref_time_ind);
+        parameter_data = ncread(url,NCML_par_list{pref_time_ind});
+        time_text = parameter_data';
+        time_data = time_text(1,1:64);                   
+        disp(['preferred_timestamp:', time_data]);
+        fprintf(fidb,'%32s,',char(time_name));
+        fprintf(fidb,'%32s,%23s,\n','listed as: ',time_data);
+    else
+        fprintf(fidb,'%32s,\n','preferred_timestamp is not listed in the file')
+    end
+    
+    fprintf(fidb,'%s,\n','____');
     fprintf(fidb,'%31s,%31s,%20s,%20s,%20s,%20s,%20s,%20s,%20s,\n',...
            'parameter_name  ','parameter_unit  ',...
            'parameter_size','min_val  ','max_val  ','isnan_val  ',...
            'iszeros_val  ','isnegatif_val  ','isFill_val');
-        
-    NCML_par_list = {meta.Variables.Name}';
-    NCML_par_type = {meta.Variables.Datatype}';
-       
+   
+      
+%         parameter_data = ncread(url,NCML_par_list{parameter_ind});
+
+
     for ii = 1:length(NCML_par_list)
         if strcmp(NCML_par_type(ii),'char') == 0
             disp(NCML_par_list(ii))
@@ -76,6 +95,7 @@ function [meta,t0,ti,tvar,file_id] = A_read_ncml(url,fidb)
                 fprintf(fidb,'%15.4f,%15.4f,%15d,%15d,%15d,%15d\n',min_val,max_val,...
                                             nan_val,zeros_val,negatif_val,Fill_val);
 
+                            
                 if strcmp(parameter_name,'time') == 1
                     tvar = ((parameter_data(:,1))/86400) + datenum(1900, 1, 1, 0, 0, 0);
                     t0 = tvar(1);
